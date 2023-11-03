455821302316c13c3ad39ca0919208c73fc0a3c6
蛰鹿 <m15006802940@163.com>
2023-11-03 09:33:24 +0000

    update static/js/config.js.
    Signed-off-by: 蛰鹿 <m15006802940@163.com>

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,3 @@
-// 编码
 let data = 'sk-HCdn3mbp7uGOAJFJal1PhlSnuroFfsr4cb2wL9drebZNeb1q';
 let encodedData = btoa(data);
 console.log(encodedData);  // 输出: aGVsbG8gd29ybGQ=
@@ -10,5 +9,5 @@ console.log(decodedData);  // 输出: hello world
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://api.chatanywhere.com.cn",  // gpt3.5代理接口
-    apiKey: "${decodedData}"  // openAi Base64 编码 apiKey
-};  
+    apiKey: decodedData  // openAi Base64 编码 apiKey
+};
